b70f3407c65b115d86956d863708c6a6928fbb1e
Edward Tsai <45326902+edwardxycai@users.noreply.github.com>
2021-09-03 03:47:49 +0000

    add callback handler in JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,24 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-];
+import React from 'react';
 
 // function List() {
 //   return list.map(function(item) {
@@ -35,8 +17,69 @@ const list = [
 //   });
 // };
 
-const List = () =>
-  list.map(item => (
+function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ];
+
+  const handleSearch = event => {
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>
+        My Hacker Stories
+      </h1>
+
+      <Search onSearch={handleSearch}/>
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+}
+
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+
+    // call callback function
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  )
+}
+
+const List = props =>
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -46,26 +89,5 @@ const List = () =>
       <span>{item.points}</span>
     </div>
   ));
-
-function App() {
-  const handleChange = event => {
-    console.log(event.target.value);
-  };
-
-  return (
-    <div>
-      <h1>
-        My Hacker Stories
-      </h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <hr />
-
-      <List />
-    </div>
-  );
-}
 
 export default App;
